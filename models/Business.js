fdb583b8d1762797632bae199337c7fc6576df12
chaitanya717 <chaudharichaitanya998@gmail.com>
2024-10-26 09:33:53 +0530

    firste

--- models/Business.js
+++ models/Business.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const hrSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    mobile: { type: String, required: true },
-    Active: { type: Boolean, default: true },
+  name: { type: String, required: true },
+  mobile: { type: String, required: true },
+  Active: { type: Boolean, default: true },
 });
 
 const businessSchema = new mongoose.Schema({
-    businessName: { type: String, required: true },
-    userid :  { type: String, required: true },
-    businessType: { type: String, required: true },
-    businessOption: { type: String, required: true },
-    HR: [hrSchema],
-    Active: { type: Boolean, default: true },
+  businessName: { type: String, required: true },
+  userid: { type: String, required: true },
+  businessType: { type: String, required: true },
+  businessOption: [],
+  HR: [hrSchema],
+  Active: { type: Boolean, default: true },
 });
 
-module.exports = mongoose.model('Business', businessSchema);
+module.exports = mongoose.model("Business", businessSchema);
