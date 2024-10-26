c707ec5ca08fbda14bf75917d82d978730159c17
chaitanya717 <chaudharichaitanya998@gmail.com>
2024-10-26 21:45:49 +0530

    second

--- models/entryBus.js
+++ models/entryBus.js
@@ -1,22 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const paymentStatusSchema = new mongoose.Schema({
-    status: { type: String, required: true },
-    mode: { type: String, required: true },
+  status: { type: String, required: true },
+  mode: { type: String, required: true },
 });
 
 const entrySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    userid: { type: String, required: true },
-    date: { type: Date, required: true },
-    mobile: { type: String, required: true },
-    bag: { type: Number, required: true },
-    rate: { type: Number, required: true },
-    hrcount: { type: Number, required: true },
-    hrname: [String],
-    diesel: { type: Number, required: true },
-    total: { type: Number, required: true },
-    paymentStatus: paymentStatusSchema
+  name: { type: String, required: true },
+  userid: { type: String, required: true },
+  date: { type: Date, required: true },
+  mobile: { type: String, required: true },
+  businessOption: { type: String, required: true },
+  bag: { type: Number, required: true },
+  rate: { type: Number, required: true },
+  hrcount: { type: Number, required: true },
+
+  hrname: [String],
+  diesel: { type: Number, required: true },
+  total: { type: Number, required: true },
+  paymentStatus: paymentStatusSchema,
 });
 
-module.exports = mongoose.model('entry_buisness', entrySchema);
+module.exports = mongoose.model("entry_buisness", entrySchema);
